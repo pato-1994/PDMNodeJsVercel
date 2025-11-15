9ac204cf2997ecf77d5cd8ef902d7fc16f19f02c
Donal Ugalde <donaldo_ugesc@hotmail.com>
2025-11-14 23:29:57 -0600

    se regresa a la anterior version del archivo

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// en: index.js
-
 import dotenv from "dotenv";
 import express from "express";
 import taskRoutes from "./routes/task.js";
@@ -7,23 +5,13 @@ import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 const app = express();
+export default app;
 
-// --- 1. MIDDLEWARE ---
-// Primero, le decimos a la app cómo leer JSON
 app.use(express.json());
-
-// --- 2. RUTAS ---
-// Ahora, le decimos qué rutas usar
 app.use("/api/tasks", taskRoutes);
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => res.send("API Task funcionando Donaldo"));
 
-// --- 3. (REMOVIDO) ---
-// Quitamos app.listen(). Vercel se encarga de esto.
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
-
-// --- 4. EXPORT (AL FINAL) ---
-// Exportamos la app YA CONFIGURADA
-export default app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));

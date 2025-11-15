98d986aed3dfcd003f2856e581b348680cf4fc03
Donal Ugalde <donaldo_ugesc@hotmail.com>
2025-11-15 00:56:34 -0600

    Se corrigio Modelo/Task.js

--- model/Task.js
+++ model/Task.js
@@ -12,21 +12,21 @@ export class Task {
     }
 
     static async create(task) {
-        const { name, plannedDate, status } = task;
+        const { name, descripcion, deadline } = task;
         const [result] = await pool.query(
-            "INSERT INTO task (name, plannedDate, status) VALUES (?, ?, ?)",
-            [name, plannedDate, status]
+            "INSERT INTO task (name, descripcion, deadline) VALUES (?, ?, ?)",
+            [name, descripcion, deadline]
         );
         return { id: result.insertId, ...task };
     }
 
     static async update(id, task) {
-        const { name, status, deadline } = task;
+        const { name, descripcion, deadline } = task;
         await pool.query(
-            "UPDATE task SET name = ?, status = ?, deadline = ? WHERE id = ?",
-            [name, status, deadline, id]
+            "UPDATE task SET name = ?, descripcion = ?, deadline = ? WHERE id = ?",
+            [name, descripcion, deadline, id]
         );
-       // return { id, ...task };
+       return { id, ...task };
     }
     static async delete(id) {
         await pool.query("DELETE FROM task WHERE id = ?", [id]);
